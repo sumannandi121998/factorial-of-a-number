b4c19641b39e5f2300cbb72f0e8952f9a98ea76d
sumannandi121998 <57460338+sumannandi121998@users.noreply.github.com>
2019-12-03 04:42:51 -0800

    Finding factorial of a number using c

--- factorial using c.c
+++ factorial using c.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <time.h>
-double f(int n)
+double f(int n) //function for calculating factorial
 {
   if (n>=1)
-    return n*f(n-1);
+    return n*f(n-1); //using recursion
   else
     return 1;
 }
 int main()
 {
   int n;
-  printf("Give a positive integer: ");
+  printf("Give the value of a positive integer: ");
   scanf("%d",&n);
   clock_t t;
   if (n<0)
-      printf("Give a positive integer");
+      printf("This is not a positive integer.Give a positive integer");
   else
+  {
     t=clock();
-    printf("The value of factorial %d is %10e\n",n,f(n));
+    printf("The value of factorial %d is %10e\n",n,f(n)); //the output calling function
     t=clock()-t;
-    double time=((double)t)/CLOCKS_PER_SEC;
+    double time=((double)t)/CLOCKS_PER_SEC; //time for execution
     printf("The time taken for execution is %10e\n",time);
+  }
   return 0;
 }
