512df28e4ce225a3e772f1b56a468af8703641eb
sumannandi121998 <57460338+sumannandi121998@users.noreply.github.com>
2019-12-03 04:57:35 -0800

    Finding factorial of a number using scipy in python

--- factorial scipy.py
+++ factorial scipy.py
@@ -1,11 +1,11 @@
 import scipy as py
 import math
 import timeit
-n1=input("Give a positive integer")
-n=int(n1)
+n1=input("Give the value of a positive integer:")
+n=int(n1) #converting the input into integer
 if n<0:
-    print ("Give another value")
+    print ("This is not a positive integer.Give a positive integer")
 else:
-    f=py.math.factorial(n)
+    f=py.math.factorial(n) #factorial using scipy
     print ("The value of factorial ",n1," is ",f)
-    print ("The time taken for execution is ",timeit.timeit())
+    print ("The time taken for execution is ",timeit.timeit()) #time for execution
