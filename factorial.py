13e9e8c56d1e6bd7512818c88cb63c0c70251114
sumannandi121998 <57460338+sumannandi121998@users.noreply.github.com>
2019-12-03 05:03:40 -0800

    factorial of a number using python but writing factorial function in c

--- factorial.py
+++ factorial.py
@@ -1,12 +1,12 @@
 from ctypes import*
-fact=CDLL('factorial.so')
+fact=CDLL('factorial.so') #so file
 def f(n):
     a=fact.f(n)
     if (a==-1):
-        return "Give another value"
+        return "This is not a positive integer.Give a positive integer"
     else:
         return a
-n1=input("Give a positive integer")
-n=int(n1)
+n1=input("Give the value of a positive integer:")
+n=int(n1) #converting input to a integer
 print ("The value of factorial ",n1," is ",f(n))
     
