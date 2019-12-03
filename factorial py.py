7de06e62bbfab9412a970e8690687188c76632c4
sumannandi121998 <57460338+sumannandi121998@users.noreply.github.com>
2019-12-03 04:52:42 -0800

    Finding factorial of a number using python

--- factorial py.py
+++ factorial py.py
@@ -1,15 +1,15 @@
 from math import*
 import timeit
-def f(n):
+def f(n): #function for calculating factorial
   if n>=1:
-    return n*f(n-1);
+    return n*f(n-1); #using recursion
   else:
     return 1;
 m=1
-n1=input("Give a positive integer:")
-n=int(n1)
+n1=input("Give the value of a positive integer:")
+n=int(n1) #converting the input into integer
 if n<0:
-    print ("Give another value")
+    print ("This is not a positive integer.Give a positive integer")
 else:
-    print ("The value of factorial ",n1," is ",f(n))
-    print ("The time taken for execution is ",timeit.timeit())
+    print ("The value of factorial ",n1," is ",f(n)) #the output by calling function
+    print ("The time taken for execution is ",timeit.timeit()) #time for execution
